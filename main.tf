85a393b41d071e7c6ca15aa68c24dbc3b318e51d
Vaidyaakash <1akashvaidya1@gmail.com>
2025-12-14 14:42:43 +0530

    updated creds

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ module "newrelic_alerts" {
   source = "./modules/alerts"
 
 
-  newrelic_api_key = var.NEW_RELIC_API_KEY
+  NEW_RELIC_API_KEY = var.NEW_RELIC_API_KEY
   # api_key          = var.newrelic_api_key
-  account_id          = var.NEW_RELIC_ACCOUNT_ID 
-  newrelic_account_id = var.NEW_RELIC_ACCOUNT_ID 
-  policy_name         = var.policy_name
-  alert_conditions    = var.alert_conditions
+  NEW_RELIC_ACCOUNT_ID = var.NEW_RELIC_ACCOUNT_ID
+  # newrelic_account_id = var.NEW_RELIC_ACCOUNT_ID 
+  policy_name      = var.policy_name
+  alert_conditions = var.alert_conditions
 }

e4133ecac2d2e50d3c735c418baf8044f3f74938
Vaidyaakash <1akashvaidya1@gmail.com>
2025-12-17 22:11:46 +0530

    alert condition added - new

--- provider.tf
+++ provider.tf
@@ -1,8 +1,19 @@
+# terraform {
+#   required_providers {
+#     newrelic = {
+#       source  = "newrelic/newrelic"
+#       version = "3.76.3"
+#     }
+#   }
+# }
+
 terraform {
+  # required_version = ">= 1.5.0"
+
   required_providers {
     newrelic = {
-      source = "newrelic/newrelic"
-      version = "3.76.3"
+      source  = "newrelic/newrelic"
+      version = "~> 3.30"
     }
   }
 }
@@ -11,5 +22,5 @@ terraform {
 provider "newrelic" {
   account_id = var.newrelic_account_id
   api_key    = var.newrelic_api_key
-  region     = "US"
+  # region     = "US"
 }
